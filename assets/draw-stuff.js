3568f40f42be4ffb3f583f7ead0bc61b62eae70e
Jonathan_Ishii <jtishii@yahoo.com>
2019-04-22 13:02:13 -0700

    Added index and cell values to the graph

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -1,11 +1,11 @@
-/* 
+/*
 Authors: Jonathan Ishii, Matthew Mikulka
 Contact: jtishii@csu.fullerton.edu, mattmikulka@csu.fullerton.edu
 Description: This program illustrates Wolfram's Rule-90 is based on a 1D array where each cell is active.
             We will illustate on a webpage visually how this works. This file holds all the functions to be called by the Cela Rule 90.html file
 */
 // Draw filled rect.
-function draw_rect( ctx, sSize, fill, x, y) 
+function draw_rect( ctx, sSize, fill, x, y)
 {
     fill = fill || 'white';
     ctx.beginPath();
@@ -21,19 +21,38 @@ function draw_rect( ctx, sSize, fill, x, y)
 
 // =====================================================  draw_grid ====
 // draw starting grid
-function draw_board( rctx, sSize, rstroke, rfill1, rfill2, boardNumbers) 
+function draw_board( rctx, sSize, rstroke, rfill1, rfill2, boardNumbers)
 {
     rctx.save( );
     rctx.strokeStyle = "black";
 	rctx.lineWidth = 1;
-	for (var iy = 0; iy < 10; iy += 1) 
+	for (var iy = 0; iy < 10; iy += 1)
 	{
+      // draws y axis cell labels
+      rctx.save();
+      rctx.font = "40px Arial";
+      rctx.fillStyle = "black";
+      rctx.fillText(iy, 10, iy * sSize + 140);
+      rctx.restore();
     	for ( var ix = 0; ix < 10; ix += 1)
     	{
 			var fill;
 			if ((iy + ix) % 2 == 0) {fill = rfill1;}
 			else {fill = rfill2}
-			draw_rect (rctx, sSize, fill, ix * sSize, iy * sSize);
+			draw_rect (rctx, sSize, fill, ix * sSize + 50, iy * sSize + 50);
+
+      // draws the value of the cells on the board
+      rctx.save();
+      rctx.context
+      rctx.font = "20px Arial";
+      rctx.fillStyle = "blue";
+      rctx.fillText(boardNumbers[iy][ix][0], ix * sSize + 55, iy * sSize + 70);
+
+      // draws x axis cell indexs
+      rctx.font = "40px Arial";
+      rctx.fillStyle = "black";
+      rctx.fillText(ix, ix * sSize + 120, 30);
+      rctx.restore();
    	 	}
 	}
 }
@@ -56,7 +75,7 @@ function createBoardArray()
 		}
 	}
 	//print array numbers to console
-	/* 
+	/*
 	for (var i = 0; i < 10; ++i)
 	{
 		for (var j = 0; j < 10; ++j)
@@ -73,5 +92,10 @@ function DFS(board_arr, path_ammount, current_y, current_x, sink_y, sink_x) {
 	var paths = new Array();
 	var tempX = current_x -1;
 	var tempY = current_y -2;
-	
+
+}
+
+function drawCellCap()
+{
+
 }
